e402ee454d14325dc796a96681ae6ec584f61e8a
unknown <dwsb@cin.ufpe.br>
2018-05-22 03:26:02 -0300

    Adicionado cadastro de usuarios

--- serverApp/app.js
+++ serverApp/app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require("body-parser");
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,8 +14,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- serverApp/routes/usuarios.js
+++ serverApp/routes/usuarios.js
@@ -17,4 +17,22 @@ router.get('/',function(req, res, next){
     });
 });
 
+router.post('/cadastrar', function(req, res, fields){
+    console.log(req.body);
+    var sql = "insert into tb_usuarios (nome, cpf, email, login, senha, localizacao_atual, rua, numero, bairro, cep, estado, pais, complemento, foto_perfil) Values ?"
+    var values = [[req.body.nome, req.body.cpf, req.body.email, req.body.login, req.body.senha, req.body.localizacao_atual,
+    req.body.rua, req.body.numero,req.body.bairro, req.body.cep, req.body.estado, req.body.pais, req.body.complemento, req.body.foto_perfil]];
+    connection.query(sql, [values], function (err, result) {
+        if (err){
+            res.send({men: "error"});
+            throw err;
+        } 
+        console.log("Number of records inserted: " + result.affectedRows);
+        res.send({men: "ok"});
+    });
+});
+
+
+
+
 module.exports = router;
